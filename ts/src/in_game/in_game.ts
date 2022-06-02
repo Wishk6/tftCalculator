3405976606115987e6b25aae753930a5e21a86df
Wishk6 <sauringuillaume@gmail.com>
2022-06-02 23:22:38 +0200

    data received / img ok

--- ts/src/in_game/in_game.ts
+++ ts/src/in_game/in_game.ts
@@ -12,15 +12,18 @@ import WindowState = overwolf.windows.WindowStateEx;
 class InGame extends AppWindow {
   private static _instance: InGame;
   private _gameEventsListener: OWGamesEvents;
-   private _eventsLog: HTMLElement;
+  private _eventsLog: HTMLElement;
+  private _boardLog: HTMLElement;
   private _damageLog: HTMLElement;
+  private _champsLog: HTMLElement;
+  public localChampsData = [];
+  public localDamageData = [];
 
   private constructor() {
     super(kWindowNames.inGame);
 
     this._damageLog = document.getElementById('dmgLog');
-
-    this._eventsLog = document.getElementById('eventsLog');
+    this._boardLog = document.getElementById('boardLog');
 
     this.setToggleHotkeyBehavior();
     this.setToggleHotkeyText();
@@ -54,15 +57,18 @@ class InGame extends AppWindow {
   private async onInfoUpdates(info: any) {
 
     if (info.match_info.local_player_damage != undefined) {
-      this.logLineDmg(info)
+      await this.logLineDmg(info)
     }
- 
+
+    if (info.board != undefined) {
+      await this.logBoard(info);
+    }
+
   }
 
   // Special events will be highlighted in the event log
   private onNewEvents(e: { events: any[]; }) {
 
-    this._eventsLog.innerHTML = JSON.stringify(e.events[0]); // gold 
   }
 
   // Displays the toggle minimize/restore hotkey in the window header
@@ -93,15 +99,41 @@ class InGame extends AppWindow {
   }
 
   // Appends a new line to the specified log
-  private logLineDmg(data: { match_info: { local_player_damage: string; }; }) {
+  private async logLineDmg(data: { match_info: { local_player_damage: string; }; }) {
+    let damageData = [];
+    let champsData = [];
+    // let dmgData = [];
+    let len = (Math.floor(data.match_info.local_player_damage.split("\"").length / 10));
+    for (let i = 0; i < len; i++) {
+      // dmgData.push(data.match_info.local_player_damage.split("\"")[5 + (i * 10)].replace(/\s|TFT6_/g, '') + (data.match_info.local_player_damage.split("\"")[8 + (i * 10)].replace(/\s|:|,/g, ' ')));
+      champsData.push(data.match_info.local_player_damage.split("\"")[5 + (i * 10)].replace(/\s|TFT6_/g, ''));
+      damageData.push(data.match_info.local_player_damage.split("\"")[8 + (i * 10)].replace(/\s|:|,/g, ' '));
+    }
+    this.localChampsData = champsData;
+    this.localDamageData = damageData;
+    this._damageLog.innerHTML = JSON.stringify(damageData).replace(/\s|,/g, ' '); // dmg
+    this._champsLog.innerHTML = JSON.stringify(damageData).replace(/\s|,/g, ' '); // dmg
 
-    let regCleaner = new RegExp('', '');
-    let dmgData = data.match_info.local_player_damage.replaceAll("\"","");
-
-    this._damageLog.innerHTML = JSON.stringify(dmgData); // dmg
-
-    // https://leagueoflegends.fandom.com/wiki/Category:TFT_item_icons
   }
+
+  private async logBoard(data: { board: { board_pieces: string; }; }) {
+
+    this._boardLog.innerHTML = JSON.stringify(data.board.board_pieces.replace(/,|:|item_1|item_2|item_3|name|cell_|TFT6_|\"/g, ' '));
+    await this.logChampsOpt();
+  }
+
+  private async logChampsOpt() {
+
+    for (let i = 0; i < this.localChampsData.length; i++) {
+      document.getElementById(i.toString()).innerHTML = this.localChampsData[i];
+    }
+    this.localChampsData.length = 0;
+    
+  }
+
+  // faire les fonctions sur le clic kdes images 'les stinrgs envoyé 
+  // faire en sorte de nettoyer la data reçue et modifée 
+  // voir pour algos si le temps
 
   private async getCurrentGameClassId(): Promise<number | null> {
     const info = await OWGames.getRunningGameInfo();
